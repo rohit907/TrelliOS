c6e675d2cad4e44a051232428bf67acfc5ec1f9a
Rohit Mishra <rohitmishra@learniosfreecoms-mac.local>
2016-07-23 12:41:43 -0400

    Initial Commit

---
+++ trell/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  trell
+//
+//  Created by Rohit Mishra on 7/23/16.
+//  Copyright © 2016 Rohit Mishra. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ trell/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  trell
+//
+//  Created by Rohit Mishra on 7/23/16.
+//  Copyright © 2016 Rohit Mishra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
